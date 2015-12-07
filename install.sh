4933a6330f1dc961ef0ed05ac6b9a3e8035f0ce8
Joe Terranova <joeterranova@gmail.com>
2015-12-07 04:19:21 +0000

    updated propel steps in install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 composer install
 bower install
 compass compile -c resources/compass/config.rb  --sass-dir resources/compass/sass/ --css-dir web/css/ --image-dir web/images/
-vendor/propel/propel/bin/propel config:convert --output-dir=src --output-file=propel-config.php
+./propel config:convert --output-dir=src --output-file=propel-config.php
+./propel sql:insert
