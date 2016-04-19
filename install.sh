ae67529d1f87408a1d2afd3f19be058125ee5e66
Joe Terranova <joeterranova@gmail.com>
2016-04-19 02:50:51 +0000

    Fixing sass files

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 composer install
 bower install
-compass compile -c resources/compass/config.rb  --sass-dir resources/compass/sass/ --css-dir web/css/ --image-dir web/images/
+compass compile -c config.rb  --sass-dir web/sass/ --css-dir web/stylesheets/ --image-dir web/images/
 ./propel config:convert --output-dir=src --output-file=propel-config.php
 ./propel sql:insert
