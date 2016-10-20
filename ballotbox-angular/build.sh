9fb75a5d2b0c4beed874bf05fe60c7c864f3008f
Joe Terranova <joeterranova@gmail.com>
2016-10-20 04:06:44 +0000

    Fixed path to index.php in build.sh

--- ballotbox-angular/build.sh
+++ ballotbox-angular/build.sh
@@ -1,2 +1,2 @@
-!/bin/bash
-ng build --dev && cp src/.htaccess ../web/.htaccess && cp src/index.php ../www/index.php
+#!/bin/bash
+ng build --dev && cp src/.htaccess ../web/.htaccess && cp src/index.php ../web/index.php
