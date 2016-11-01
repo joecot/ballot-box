6b71dc133cc571a111684758e41efdd4e0ad9e9a
Joe Terranova <joeterranova@gmail.com>
2016-11-01 03:54:17 +0000

    Adding routing

---
+++ ballotbox-angular/src/app/ballot/ballot.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { BallotComponent } from './ballot.component';
+
+describe('BallotComponent', () => {
+  let component: BallotComponent;
+  let fixture: ComponentFixture<BallotComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BallotComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BallotComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ballotbox-angular/src/app/ballot/ballot.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BallotComponent } from './ballot.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BallotComponent]
+})
+export class BallotModule { }
