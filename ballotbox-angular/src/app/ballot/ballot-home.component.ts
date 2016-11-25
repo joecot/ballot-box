4477a2610998cb9e704bfe1c27d4d27a997e6e60
robotnicka <sheswithoutwings@gmail.com>
2016-11-25 23:04:46 +0000

    Change default sentence on ballot page.

--- ballotbox-angular/src/app/ballot/ballot-home.component.ts
+++ ballotbox-angular/src/app/ballot/ballot-home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ballot-home',
-  template: 'Pick a ballot or add a new one to the left!',
+  template: 'Select an existing ballot or create a new one.',
 })
 export class BallotHomeComponent implements OnInit {
 
